faa7d3e8eab65537aa9603a17a47a5c0f3e1867b
Gaeel Bradshaw <gaeel.bradshaw@goto-games.com>
2022-02-02 11:27:52 +0100

    fix(slack): always 200 slack hooks

--- api/slackhook.js
+++ api/slackhook.js
@@ -14,12 +14,12 @@ router.post('/', function (req, res, next) {
 			break;
 		case "event_callback":
 			if (req.body.token !== config.slack.verification) {
-				return res.status(403).send("Invalid token");
+				return res.status(200).send("Invalid token");
 			}
 			if (req.body.hasOwnProperty('event')) {
 				let match = commandRgx.exec(req.body.event.text);
 				if (match === null) {
-					return res.status(400).send("Could not find command");
+					return res.status(200).send("Could not find command");
 				}
 				switch (match.groups.command){
 					case 'help':
@@ -38,13 +38,13 @@ router.post('/', function (req, res, next) {
 						cancel.cancel(match.groups.command, match.groups.project, function (err) {
 							if (err) {
 								console.log("buildCancel error : " + err);
-								return res.status(500).send("Unable to cancel build");
+								return res.status(200).send("Unable to cancel build");
 							} else {
 								return res.status(200).send("Build canceled");
 							}
 						});
 					default:
-						return res.status(400).send("Unknown command");
+						return res.status(200).send("Unknown command");
 				}
 			}
 			break;
